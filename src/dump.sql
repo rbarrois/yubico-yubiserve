06caccc4882ebdbb93ef0d4a0f6650e692d4443a
b1galez <b1galez@fbcee277-3294-991b-8290-beb7048acdd6>
2010-11-20 23:19:51 +0000

    Version 2.0
    git-svn-id: http://yubico-yubiserve.googlecode.com/svn/trunk@4 fbcee277-3294-991b-8290-beb7048acdd6

--- src/dump.sql
+++ src/dump.sql
@@ -1,9 +1,25 @@
+BEGIN TRANSACTION;
 create table yubikeys(
-	publicname varchar(16) unique not null,
-	created varchar(24) not null,
-	internalname varchar(12) not null,
-	aeskey varchar(32) not null,
-	active boolean default true,
-	counter integer not null default 1,
-	time integer not null  default 1
+ nickname varchar(16) unique not null,
+ publicname varchar(16) unique not null,
+ created varchar(24) not null,
+ internalname varchar(12) not null,
+ aeskey varchar(32) not null,
+ active boolean default true,
+ counter integer not null default 1,
+ time integer not null default 1
 );
+create table oathtokens(
+ nickname varchar(16) unique not null,
+ publicname varchar(12) unique not null,
+ created varchar(24) not null,
+ secret varchar(40) not null,
+ active boolean default true,
+ counter integer not null default 1
+);
+create table apikeys(
+ nickname varchar(16),
+ secret varchar(28),
+ id integer primary key
+);
+COMMIT;
